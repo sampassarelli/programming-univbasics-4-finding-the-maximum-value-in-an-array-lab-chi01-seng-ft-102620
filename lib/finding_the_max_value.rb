d173267922a30f9a08f05b2c769ec61aa79b37ec
Sam Passarelli <passarelli.sam@gmail.com>
2020-09-09 20:02:28 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -2,6 +2,7 @@ def find_max_value(array)
   largest = 0
   for x in range(0, len(array)):
     if(array[x] > largest):
-            largest = array[x]
+        largest = array[x]
     return largest
+  end
 end
